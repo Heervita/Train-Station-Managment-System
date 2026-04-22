6bb85843f3169949c0598c5aea927f7a9d938ff4
Heervita Mistri <heervitamistri6@gmail.com>
2026-04-22 13:14:14 +0530

    database set up

--- TrainStation.java
+++ TrainStation.java
@@ -1,3 +1,16 @@
-public class TrainStation {
+import javax.swing.*;
+
+public class TrainStation extends JFrame{
+
+    //Database configiraton
+    static final String db_url  = "url";
+    static final String db_user = "root";
+    static final String db_password = "password";
     
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (Exception ignored){}
+    }
 }
